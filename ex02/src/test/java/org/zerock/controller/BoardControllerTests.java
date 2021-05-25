f1a28dd46c3c7297db9b3a0c11d84501d2da2505
bbane <ssj7410@gmail.com>
2021-05-25 10:32:39 +0900

    5/25수정

---
+++ ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -0,0 +1,8 @@
+package org.zerock.controller;
+
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@WebAppConfiguration//테스트할 DI컨테이너를 웹 어플리케이션 전용 DI컨테이너로 처리
+public class BoardControllerTests {
+
+}
